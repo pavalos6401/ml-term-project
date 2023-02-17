09554bcf9fe6996f6beec71efebc21640cbf21bc
Pedro Avalos <pedro.avalos@proton.me>
2023-02-16 19:56:31 -0600

    Make empty util module.

---
+++ util.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module provies utilities for using/manipulating the dataset."""
+
+from PIL import Image
+from sklearn.utils import Bunch
+
+
+def load_star_galaxy_dataset() -> Bunch:
+    """Loads and returns the star-galaxy dataset (classification).
+
+    Returns:
+        data (Bunch): Dictionary-like object.
+    """
+
+    pass
