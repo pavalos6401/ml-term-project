d17479379890f901cbf7b7b21fd9df78eac4be66
unknown <philip.rhoda@my.wheaton.edu>
2023-04-12 22:24:45 -0500

    random tests

--- src/util.py
+++ src/util.py
@@ -11,7 +11,7 @@ _MODULE_PATH = Path(__file__).parent
 """Parent directory of this python module."""
 
 
-def __load_class(container_path: Path, dataset: dict, target: int) -> None:
+def __load_class_training(container_path: Path, dataset: dict, target: int) -> None:
     """Helper function to load all files of a single classification, in-place.
 
     Args:
@@ -19,8 +19,31 @@ def __load_class(container_path: Path, dataset: dict, target: int) -> None:
         dataset (dict): Dataset instance to append data to.
         target (int): The target classification of the data being loaded.
     """
-
+    i = 0
     for file in container_path.iterdir():
+        if i == 500:
+            break
+        i = i + 1
+        image: Image.Image = Image.open(file).convert("L")
+        data: ArrayLike = np.asarray(image).flatten() / 255.0
+
+        dataset["filename"].append(file.name)
+        dataset["data"].append(data)
+        dataset["target"].append(target)
+
+def __load_class_test(container_path: Path, dataset: dict, target: int) -> None:
+    """Helper function to load all files of a single classification, in-place.
+
+    Args:
+        container_path (Path): Path to directory containing the data files.
+        dataset (dict): Dataset instance to append data to.
+        target (int): The target classification of the data being loaded.
+    """
+    i = -1
+    for file in container_path.iterdir():
+        i = i + 1
+        if i < 500:
+            continue
         image: Image.Image = Image.open(file).convert("L")
         data: ArrayLike = np.asarray(image).flatten() / 255.0
 
@@ -29,7 +52,7 @@ def __load_class(container_path: Path, dataset: dict, target: int) -> None:
         dataset["target"].append(target)
 
 
-def load_star_galaxy_dataset() -> Bunch:
+def load_star_galaxy_dataset() -> (Bunch, Bunch):
     """Loads and returns the star-galaxy dataset (classification).
 
     Returns:
@@ -43,6 +66,66 @@ def load_star_galaxy_dataset() -> Bunch:
     """
 
     # Set up base dataset without the data
+    dataset_train: dict = {}
+    dataset_train["DESCR"] = (
+        "This is a simple dataset consisting of ~3000 64x64 images of stars "
+        "and ~1000 images of galaxies. The images were captured by the "
+        "in-house 1.3m telescope of the observatory situated in Devasthal, "
+        "Nainital, India."
+    )
+    dataset_train["filename"] = []
+    dataset_train["data"] = []
+    dataset_train["target"] = []
+    dataset_train["target_names"] = np.asarray(["star", "galaxy"])
+
+    # Load each class of data into the dataset
+    dataset_path: Path = _MODULE_PATH / "dataset"
+    for target, target_name in enumerate(dataset_train["target_names"]):
+        __load_class_training(
+            container_path=(dataset_path / target_name),
+            dataset=dataset_train,
+            target=target,
+        )
+
+    # Convert dataset attributes to numpy arrays
+    dataset_train["filename"] = np.asarray(dataset_train["filename"])
+    dataset_train["data"] = np.asarray(dataset_train["data"])
+    dataset_train["target"] = np.asarray(dataset_train["target"])
+
+
+
+
+
+    dataset_test: dict = {}
+    dataset_test["DESCR"] = (
+        "This is a simple dataset consisting of ~3000 64x64 images of stars "
+        "and ~1000 images of galaxies. The images were captured by the "
+        "in-house 1.3m telescope of the observatory situated in Devasthal, "
+        "Nainital, India."
+    )
+    dataset_test["filename"] = []
+    dataset_test["data"] = []
+    dataset_test["target"] = []
+    dataset_test["target_names"] = np.asarray(["star", "galaxy"])
+
+    # Load each class of data into the dataset
+    dataset_path: Path = _MODULE_PATH / "dataset"
+    for target, target_name in enumerate(dataset_test["target_names"]):
+        __load_class_test(
+            container_path=(dataset_path / target_name),
+            dataset=dataset_test,
+            target=target,
+        )
+
+    # Convert dataset attributes to numpy arrays
+    dataset_test["filename"] = np.asarray(dataset_test["filename"])
+    dataset_test["data"] = np.asarray(dataset_test["data"])
+    dataset_test["target"] = np.asarray(dataset_test["target"])
+
+    return (Bunch(**dataset_train), Bunch(**dataset_test))
+
+def load_star_gal_training() -> Bunch:
+
     dataset: dict = {}
     dataset["DESCR"] = (
         "This is a simple dataset consisting of ~3000 64x64 images of stars "
@@ -58,11 +141,21 @@ def load_star_galaxy_dataset() -> Bunch:
     # Load each class of data into the dataset
     dataset_path: Path = _MODULE_PATH / "dataset"
     for target, target_name in enumerate(dataset["target_names"]):
-        __load_class(
-            container_path=(dataset_path / target_name),
-            dataset=dataset,
-            target=target,
-        )
+        # __load_class_training(
+        container_path=(dataset_path / target_name),
+        #     dataset=dataset,
+        #     target=target,
+        # )
+        i = 0
+        for file in container_path.iterdir():
+            if i == 500:
+                break;
+            image: Image.Image = Image.open(file).convert("L")
+            data: ArrayLike = np.asarray(image).flatten() / 255.0
+
+            dataset["filename"].append(file.name)
+            dataset["data"].append(data)
+            dataset["target"].append(target)
 
     # Convert dataset attributes to numpy arrays
     dataset["filename"] = np.asarray(dataset["filename"])
